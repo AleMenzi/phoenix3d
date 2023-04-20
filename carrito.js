38e59d4d6983349e2385702aa9eb213ec9468f11
AleMenzi <hectoralejandro_95@hotmail.com>
2023-04-20 18:09:52 -0300

    agregado de 2 nuevos JS con funcionalidad a HTML - Carrito y HTML - Productos

---
+++ carrito.js
@@ -0,0 +1,25 @@
+let prodCarrito = [];
+
+function cargarProdLS(){
+    return JSON.parse(localStorage.getItem("productos")) || [];
+}
+
+prodCarrito = cargarProdLS();
+console.log(prodCarrito);
+
+function cargarCarrito(){
+    prodCarrito.forEach((addProd)=> {
+        const divCar = document.getElementById("box_carrito");
+        const imgCar = document.createElement("img")
+        const divTitleCar = document.createElement("div")
+        imgCar.src = addProd.image
+        imgCar.className = "img__carrito"
+        divTitleCar.innerText = addProd.title
+        divTitleCar.className = "txt__carrito"
+        divCar.appendChild(imgCar)
+        divCar.appendChild(divTitleCar)
+        
+    })
+}
+
+cargarCarrito();
